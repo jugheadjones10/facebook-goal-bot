b20d370727e60c97f3cfd123beb0e468a27efe98
YJ{ <kimyoungjin1001@gmail.com>
2018-12-11 23:24:16 +0800

    ss

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function handlePostback(sender_psid, received_postback) {}
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
      // Construct the message body
-    let message = {
+    let request_body = {
         "recipient": {
         "id": sender_psid
         },
@@ -40,7 +40,7 @@ function callSendAPI(sender_psid, response) {
     request(
         {
             "uri": "https://graph.facebook.com/v2.6/me/messages",
-            "qs": { "access_token": PAGE_ACCESS_TOKEN },
+            "qs": { "access_token": "EAAHZCbQCoCS4BAKGQoWqEE9WoavLj3eP3wOgSHikNGylf0y6ktZAVxhXHdairO9g6ZC4x45giKmBzCsnBJhpQUZAPCTxUxG1ovMNmLhN7WchTX58ttLi5ihywoO2ZB9DrU45cV1ZCOU6obb2LZAZAaptGG4XOYQn3pGZAaxU4tfZCZAj7vH9MpvxL1f"},
             "method": "POST",
             "json": request_body
         }, (err, res, body) => {
@@ -67,16 +67,16 @@ app.post('/webhook', (req, res) => {
             let webhook_event = entry.messaging[0]
             console.log(webhook_event)
 
-            // let sender_psid = webhook_event.sender.id;
-            // console.log('Sender PSID: ' + sender_psid);
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
 
-            // // Check if the event is a message or postback and
-            // // pass the event to the appropriate handler function
-            // if (webhook_event.message) {
-            //     handleMessage(sender_psid, webhook_event.message);
-            // } else if (webhook_event.postback) {
-            //     handlePostback(sender_psid, webhook_event.postback);
-            // }
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         })
 
         // Returns a '200 OK' response to all requests
