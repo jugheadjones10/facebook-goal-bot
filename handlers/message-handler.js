f7bbae92bd74e78b14ae668e2451b591f3d4f41c
YJ{ <kimyoungjin1001@gmail.com>
2018-12-12 17:24:23 +0800

    adaw

--- handlers/message-handler.js
+++ handlers/message-handler.js
@@ -6,49 +6,48 @@ function handleMessage(sender_psid, received_message) {
     // Check if the message contains text
     if (received_message.text) {
 
-        if(received_message.quick_reply){
+        if(received_message.text === "Get Started"){
             response = {
-                "text": "Alrighty, we will send you your daily goal setter at 5am every morning"
+                "text": "Welcome to the GVH goals manager bot! At which time in the morning would you like to set your daily goals?",
+                "quick_replies":[
+                    {
+                        "content_type":"text",
+                        "title":"5am",
+                        "payload":"5am"
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"6am",
+                        "payload":"6am"
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"7am",
+                        "payload":"7am"
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"8am",
+                        "payload":"8am"
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"9am",
+                        "payload":"9am"
+                    }               
+                ]
             }
         }
 
-        // Create the payload for a basic text message
-       
+        if(received_message.quick_reply){
+            response = {
+                "text": `Alrighty, we will send you your daily goal setter at ${received_message.quick_reply.payload} every morning`
+            }
+        }
 
     }else if(received_message.attachments){
-        
         // Gets the URL of the message attachment
         let attachment_url = received_message.attachments[0].payload.url
-        response = {
-            "text" : "Welcome to your daily and weekly goals manager! At which time in the morning would you like to set your daily goals?",
-            "quick_replies":[
-                {
-                    "content_type":"text",
-                    "title":"5am",
-                    "payload":"5am"
-                },
-                {
-                    "content_type":"text",
-                    "title":"6am",
-                    "payload":"6am"
-                },
-                {
-                    "content_type":"text",
-                    "title":"7am",
-                    "payload":"7am"
-                },
-                {
-                    "content_type":"text",
-                    "title":"8am",
-                    "payload":"8am"
-                },
-                {
-                    "content_type":"text",
-                    "title":"9am",
-                    "payload":"9am"
-                }               
-            ]
-        }
         // response = {
         //     "attachment": {
         //         "type": "template",
--- index.js
+++ index.js
@@ -13,20 +13,15 @@ const PAGE_ACCESS_TOKEN= "EAAHZCbQCoCS4BAKGQoWqEE9WoavLj3eP3wOgSHikNGylf0y6ktZAV
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
     let body = req.body
-
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
-
         // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function(entry) {
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0]
-            console.log(webhook_event)
-
-            let sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
-
+            let sender_psid = webhook_event.sender.id
+    
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
             if (webhook_event.message) {
