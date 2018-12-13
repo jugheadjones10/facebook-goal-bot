51546334478c5eafd5cd9da6c30d678d834078f2
YJ{ <kimyoungjin1001@gmail.com>
2018-12-14 00:47:03 +0800

    awd

--- event-response-pairs/send_morning-time__send_night_time.js
+++ event-response-pairs/send_morning-time__send_night_time.js
@@ -3,42 +3,44 @@ var {careSetting, careDaily, careWeekly} = require("./../mongoose-schemas/one")
 var {callSendAPI} = require("./../handlers/callSendAPI")
 
 function send_morning_time__send_night_time(received_message, sender_psid){
-
-    if(received_message.quick_reply.payload === "morning-time"){
-        careSetting.findOneAndUpdate({sender_PSID: sender_psid}, {$set: {morning_time: received_message.text}}).then((doc) => {
-            console.log("success")
-            }, (e) => {
-            console.log("ERROR")
+    if(received_message.quick_reply){
+        if(received_message.quick_reply.payload === "morning-time"){
+            careSetting.findOneAndUpdate({sender_PSID: sender_psid}, {$set: {morning_time: received_message.text}}).then((doc) => {
+                console.log("success")
+                }, (e) => {
+                console.log("ERROR")
+                }
+            )
+        
+            var response = {
+                "text": `Alrighty, we will send you your daily goal setter at ${received_message.text} every morning. When would you like to tick your daily tasks?`,
+                "quick_replies":[
+                    {
+                        "content_type":"text",
+                        "title":"8pm",
+                        "payload":"night-time"
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"9pm",
+                        "payload":"night-time"
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"10pm",
+                        "payload":"night-time"
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"11pm",
+                        "payload":"night-time"
+                    }         
+                ]
             }
-        )
     
-        var response = {
-            "text": `Alrighty, we will send you your daily goal setter at ${received_message.text} every morning. When would you like to tick your daily tasks?`,
-            "quick_replies":[
-                {
-                    "content_type":"text",
-                    "title":"8pm",
-                    "payload":"night-time"
-                },
-                {
-                    "content_type":"text",
-                    "title":"9pm",
-                    "payload":"night-time"
-                },
-                {
-                    "content_type":"text",
-                    "title":"10pm",
-                    "payload":"night-time"
-                },
-                {
-                    "content_type":"text",
-                    "title":"11pm",
-                    "payload":"night-time"
-                }         
-            ]
+            callSendAPI(sender_psid, response);  
         }
 
-        callSendAPI(sender_psid, response);  
     }
 
 }
