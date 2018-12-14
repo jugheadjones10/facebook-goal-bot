1cbac20f24b1e74aed0c3696f700b4db72836afb
YJ{ <kimyoungjin1001@gmail.com>
2018-12-14 16:32:34 +0800

    AWDAW

--- handlers/message-handler.js
+++ handlers/message-handler.js
@@ -5,6 +5,7 @@ var {get_started__send_morning_time} = require("./../event-response-pairs/get_st
 var {send_morning_time__send_night_time} = require("./../event-response-pairs/send_morning-time__send_night_time")
 var {send_night_time__send_year_goal} = require("./../event-response-pairs/send_night_time__send_year_goal")
 var {send_year_goal__send_week_goal} = require("./../event-response-pairs/send_year_goal__send_week_goal")
+var {send_week_goal__send_concluding_message} = require("./../event-response-pairs/send_week_goal__send_concluding_message")
 
 function handleMessage(sender_psid, received_message) {
 
@@ -21,7 +22,8 @@ function handleMessage(sender_psid, received_message) {
             get_started__send_morning_time(received_message, sender_psid),
             send_morning_time__send_night_time(received_message, sender_psid),
             send_night_time__send_year_goal(received_message, sender_psid),
-            send_year_goal__send_week_goal(received_message, sender_psid)
+            send_year_goal__send_week_goal(received_message, sender_psid),
+            send_week_goal__send_concluding_message(received_message, sender_psid)
         ]
 
         hey.forEach(function(res){
