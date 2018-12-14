dad8f87d467146cf85171275a16afd5046a5f54a
YJ{ <kimyoungjin1001@gmail.com>
2018-12-14 15:30:03 +0800

    Wd

--- handlers/message-handler.js
+++ handlers/message-handler.js
@@ -4,16 +4,15 @@ var {careSetting, careDaily, careWeekly} = require("./../mongoose-schemas/one")
 var {get_started__send_morning_time} = require("./../event-response-pairs/get_started__send-morning_time")
 var {send_morning_time__send_night_time} = require("./../event-response-pairs/send_morning-time__send_night_time")
 var {send_night_time__send_year_goal} = require("./../event-response-pairs/send_night_time__send_year_goal")
-//var {send_year_goal__send_week_goal} = require("./../event-response-pairs/send_year_goal__send_week_goal")
+var {send_year_goal__send_week_goal} = require("./../event-response-pairs/send_year_goal__send_week_goal")
 
 function handleMessage(sender_psid, received_message) {
 
     // Check if the message contains text
     if (received_message.text) {
        
-        callSendAPI(sender_psid, get_started__send_morning_time(received_message, sender_psid)  ||  send_morning_time__send_night_time(received_message, sender_psid) ||  send_night_time__send_year_goal(received_message, sender_psid))
+        callSendAPI(sender_psid, get_started__send_morning_time(received_message, sender_psid)  ||  send_morning_time__send_night_time(received_message, sender_psid) ||  send_night_time__send_year_goal(received_message, sender_psid) ||  send_year_goal__send_week_goal(received_message, sender_psid))
       
-        // ||  send_year_goal__send_week_goal(received_message, sender_psid)
         
         //Why doesn't the below work?
         // var hey = [
