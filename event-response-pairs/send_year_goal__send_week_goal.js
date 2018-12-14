7cefc77f41b1f8a5bf6cb66e0705463ba957e55a
YJ{ <kimyoungjin1001@gmail.com>
2018-12-14 15:39:56 +0800

    Awd

--- event-response-pairs/send_year_goal__send_week_goal.js
+++ event-response-pairs/send_year_goal__send_week_goal.js
@@ -2,26 +2,29 @@ const {mongoose} = require("./../database/mongoose")
 var {careSetting, careDaily, careWeekly} = require("./../mongoose-schemas/one")
 
 function send_year_goal__send_week_goal(received_message, sender_psid){
+    if(received_message.text.split(" ").includes("2019")){
+        
+        var checker = received_message.text.split("2019", 1)
 
-    var checker = received_message.text.split("2019", 1)
-
-    if(checker[0] === "My goal for "){
-        careSetting.findOneAndUpdate({sender_PSID: sender_psid}, {$set: {yearly_goal: received_message.text}}).then((doc) => {
-            console.log("success")
-            }, (e) => {
-            console.log("ERROR")
+        if(checker[0] === "My goal for "){
+            careSetting.findOneAndUpdate({sender_PSID: sender_psid}, {$set: {yearly_goal: received_message.text}}).then((doc) => {
+                console.log("success")
+                }, (e) => {
+                console.log("ERROR")
+                }
+            )
+        
+            var response
+            return response = {
+                "text" : "Awesome. Let's start with week 1. What smaller goal do you have for this coming week that will help you achieve your yearly goal?"
             }
-        )
     
-        var response
-        return response = {
-            "text" : "Awesome. Let's start with week 1. What smaller goal do you have for this coming week that will help you achieve your yearly goal?"
+        }else{
+            return undefined
         }
-
     }else{
         return undefined
     }
-
 }
 
 module.exports = {send_year_goal__send_week_goal}
