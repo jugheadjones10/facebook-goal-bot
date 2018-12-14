2df992946732eef0b8d0919432f1a7651419bd1e
YJ{ <kimyoungjin1001@gmail.com>
2018-12-14 15:24:44 +0800

    Awd

--- handlers/message-handler.js
+++ handlers/message-handler.js
@@ -11,7 +11,8 @@ function handleMessage(sender_psid, received_message) {
     // Check if the message contains text
     if (received_message.text) {
        
-        callSendAPI(sender_psid, get_started__send_morning_time(received_message, sender_psid) ||  send_morning_time__send_night_time(received_message, sender_psid) ||  send_night_time__send_year_goal(received_message, sender_psid))
+        callSendAPI(sender_psid, get_started__send_morning_time(received_message, sender_psid))
+        //||  send_morning_time__send_night_time(received_message, sender_psid) ||  send_night_time__send_year_goal(received_message, sender_psid)
         // ||  send_year_goal__send_week_goal(received_message, sender_psid)
         
         //Why doesn't the below work?
