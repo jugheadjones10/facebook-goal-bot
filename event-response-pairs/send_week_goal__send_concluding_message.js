d9216de2b6ca2bb09e4279b34de61d18a8d5fa79
YJ{ <kimyoungjin1001@gmail.com>
2018-12-17 13:33:57 +0800

    AWDADW

--- event-response-pairs/send_week_goal__send_concluding_message.js
+++ event-response-pairs/send_week_goal__send_concluding_message.js
@@ -48,11 +48,13 @@ function send_week_goal__send_concluding_message(received_message, sender_psid){
             mornTime.then((moTime) => {
                 console.log(moTime)
                 // var futstartMoment = moment([2018, 11, 17, moTime])
-                var futstartMoment = moment([2018, 11, 17, 13, 32])
+                var futstartMoment = moment([2018, 11, 17, 13, 37])
                 var theInterval =  futstartMoment.diff(moment(), "seconds") * 1000
-                var intervalID = global.setTimeout(myCallback, theInterval);
+                console.log(theInterval)
+                var intervalID = global.setTimeout(myCallback, theInterval)
                 function myCallback() {
                     // callSendAPI()
+                    console.log("The callback ran")
                     dayTrainStarter(sender_psid, moTime)
                 }
             })
