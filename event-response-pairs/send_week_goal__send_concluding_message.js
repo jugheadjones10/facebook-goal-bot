374ad4246f81ebdfc0bb1c8638027a274dc45324
YJ{ <kimyoungjin1001@gmail.com>
2018-12-17 12:29:00 +0800

    AWD

--- event-response-pairs/send_week_goal__send_concluding_message.js
+++ event-response-pairs/send_week_goal__send_concluding_message.js
@@ -46,7 +46,8 @@ function send_week_goal__send_concluding_message(received_message, sender_psid){
             })
             
             mornTime.then((moTime) => {
-                var futstartMoment = moment([2018, 11, 17, moTime])
+                // var futstartMoment = moment([2018, 11, 17, moTime])
+                var futstartMoment = moment([2018, 11, 17, 12, 30])
                 var theInterval =  futstartMoment.diff(moment(), "seconds") * 1000
                 var intervalID = global.setTimeout(myCallback, theInterval);
                 function myCallback() {
