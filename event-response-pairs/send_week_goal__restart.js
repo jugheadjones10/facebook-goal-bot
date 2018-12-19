f96f0e8a7784040bb440be1016f724d381582a40
YJ{ <kimyoungjin1001@gmail.com>
2018-12-19 14:58:05 +0800

    WAD

--- event-response-pairs/send_week_goal__restart.js
+++ event-response-pairs/send_week_goal__restart.js
@@ -12,27 +12,29 @@ function send_week_goal__restart(received_message, sender_psid){
             //var week_of_year = (moment().dayOfYear() + 7)/7
             var week_of_year = 2
 
-            doc.myWeekDetails.forEach(function(ele){
-                if(ele.week_number ===  week_of_year){
-                    ele.week_goal = received_message.text
+            careWeekly.findOne({sender_PSID: sender_psid}).then((doc) => {
+                doc.myWeekDetails.forEach(function(ele){
+                    if(ele.week_number ===  week_of_year){
+                        ele.week_goal = received_message.text
 
-                    doc.save().then((doc) => {
-                        console.log("success")
-                    }, (e) => {
-                        console.log("ERROR")
-                    })
-                }else{
-                    doc.myWeekDetails.push({
-                        "week_number" : week_of_year,
-                        "week_goal" : received_message.text
-                    })
-    
-                    doc.save().then((doc) => {
-                        console.log("success")
-                    }, (e) => {
-                        console.log("ERROR")
-                    })
-                }
+                        doc.save().then((doc) => {
+                            console.log("success")
+                        }, (e) => {
+                            console.log("ERROR")
+                        })
+                    }else{
+                        doc.myWeekDetails.push({
+                            "week_number" : week_of_year,
+                            "week_goal" : received_message.text
+                        })
+        
+                        doc.save().then((doc) => {
+                            console.log("success")
+                        }, (e) => {
+                            console.log("ERROR")
+                        })
+                    }
+                })
             })
 
             // careWeekly.findOne({sender_PSID: sender_psid}).then((doc) => {
