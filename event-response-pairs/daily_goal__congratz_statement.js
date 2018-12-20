0e9ce46fd1e7b4a0b0a61ad633ec52cc3b0812c0
YJ{ <kimyoungjin1001@gmail.com>
2018-12-20 17:05:42 +0800

    AAWDA

--- event-response-pairs/daily_goal__congratz_statement.js
+++ event-response-pairs/daily_goal__congratz_statement.js
@@ -9,48 +9,51 @@ function daily_goal__congratz_statement(received_message, sender_psid){
     for(i=0; i < spaceMsg.length; i++){
         news = news + spaceMsg[i]
     }
-    var targ = news.split(":")[0]
 
-    if(targ[0] === "1"){
-
-        careDaily.findOne({sender_PSID: sender_psid}).then((doc) => {
-            if(!doc){
-                var newUser = new careDaily({sender_PSID: sender_psid})
-                var doc2 = newUser.save().then((doc) => {
-                        return doc
-                        console.log("success")
-                    }, (e) => {
-                        console.log("ERROR")
-                    }
-                )
-                return doc2
-            }else{
-                return doc
-            }
-        },(e) => {
-            console.log(e)
-        }).then((doc) => {
-            //Add verification that myDayDetails with today's day_of_year does not
-            //already exist
-            doc.myDayDetails.push({
-                "day_of_year" : moment().dayOfYear(),
-                "daily_goals" : received_message.text
-            })
-
-            doc.save().then((doc) => {
-                console.log("success")
-            }, (e) => {
+    if(news.split("").includes(":")){
+        if(news.split("")[0] === "1"){
+            careDaily.findOne({sender_PSID: sender_psid}).then((doc) => {
+                if(!doc){
+                    var newUser = new careDaily({sender_PSID: sender_psid})
+                    var doc2 = newUser.save().then((doc) => {
+                            return doc
+                            console.log("success")
+                        }, (e) => {
+                            console.log("ERROR")
+                        }
+                    )
+                    return doc2
+                }else{
+                    return doc
+                }
+            },(e) => {
+                console.log(e)
+            }).then((doc) => {
+                //Add verification that myDayDetails with today's day_of_year does not
+                //already exist
+                doc.myDayDetails.push({
+                    "day_of_year" : moment().dayOfYear(),
+                    "daily_goals" : received_message.text
+                })
+    
+                doc.save().then((doc) => {
+                    console.log("success")
+                }, (e) => {
+                    console.log(e)
+                })
+            }, (e) =>{
                 console.log(e)
             })
-        }, (e) =>{
-            console.log(e)
-        })
+        
+            var response
+            return response = {
+                "text" : "Nice! Work hard! I'll follow up at the end of the day!"
+            }
     
-        var response
-        return response = {
-            "text" : "Nice! Work hard! I'll follow up at the end of the day!"
+        }else{
+            return undefined
         }
-
+    
     }else{
         return undefined
     }
